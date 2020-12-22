65b1e01c685ca29a09e34c5b0fb4abd77a99711a
dariadomagala <daria.domagala@sap.com>
2020-12-22 09:04:13 +0100

    Fix deployments endpoints

--- endpoints/deployments.js
+++ endpoints/deployments.js
@@ -8,28 +8,28 @@ import {
 export default function createDeploymentEndpoints(kubeconfig, app) {
   createGenericListEndpoint(kubeconfig, app)(
     "/namespaces/:namespace/deployments",
-    `${kubeconfig.getCurrentCluster().server}/api/v1/namespaces/{namespace}/deployments`,
+    `${kubeconfig.getCurrentCluster().server}/apis/apps/v1/namespaces/{namespace}/deployments`,
     true,
-    { kind: "Deployment", apiVersion: "v1" }
+    { kind: "Deployment", apiVersion: "apps/v1" }
   );
 
   createGenericJsonUpdateEndpoint(kubeconfig, app)(
     "/namespaces/:namespace/deployments/:name",
-    `${kubeconfig.getCurrentCluster().server}/api/v1/namespaces/{namespace}/deployments/{name}`
+    `${kubeconfig.getCurrentCluster().server}/apis/apps/v1/namespaces/{namespace}/deployments/{name}`
   );
 
   createGenericDeleteEndpoint(kubeconfig, app)(
     "/namespaces/:namespace/deployments/:name",
-    `${kubeconfig.getCurrentCluster().server}/api/v1/namespaces/{namespace}/deployments/{name}`
+    `${kubeconfig.getCurrentCluster().server}/apis/apps/v1/namespaces/{namespace}/deployments/{name}`
   );
 
   createGenericSubscriptionEndpoint(app)(
     "deployments",
-    `${kubeconfig.getCurrentCluster().server}/api/v1/namespaces/{namespace}/deployments?watch=true`,
+    `${kubeconfig.getCurrentCluster().server}/apis/apps/v1/namespaces/{namespace}/deployments?watch=true`,
     true,
     {
       kind: "Deployment",
-      apiVersion: "v1",
+      apiVersion: "apps/v1",
     }
   );
 }
