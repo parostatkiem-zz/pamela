a66164cde6507e3a02ae23bf574555ea9555e21e
Wawrzyn321 <Wawrzyn321@gmail.com>
2020-12-23 13:10:12 +0100

    Improve error handling

--- endpoints/generic.js
+++ endpoints/generic.js
@@ -4,8 +4,8 @@ export function createGenericCreateEndpoint(client, app) {
       const response = await client.create(req.body);
       res.send(response.body);
     } catch (e) {
-      console.warn(e);
-      res.status(500).send('');
+      console.error(e);
+      res.status(500).send(e.body);
     }
   });
 }
