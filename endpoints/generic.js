cff04b686f076ddc0ea306fd8097b1e671f52893
Wawrzyn321 <Wawrzyn321@gmail.com>
2020-12-29 22:39:48 +0100

    Send status code along with successfull response

--- endpoints/generic.js
+++ endpoints/generic.js
@@ -1,8 +1,8 @@
 export function createGenericCreateEndpoint(client, app) {
   app.post("/resource", async (req, res) => {
     try {
-      const response = await client.create(req.body);
-      res.send(response.body);
+      const result = await client.create(req.body);
+      res.status(result.response.statusCode).send(result.body);
     } catch (e) {
       console.error(e);
       res.status(500).send(e.body);
