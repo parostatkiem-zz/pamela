6e42ef5db5946f9a687664b9fe3bda1d52f56435
Jan Sudczak <jan.sudczak@sap.com>
2021-01-11 09:28:31 +0100

    Fix GitHub token impersonation (#10)
    * Verify tokens also with groups
    
    * improve the code
    
    * improve  error 500 messages

--- utils/headerInjector.js
+++ utils/headerInjector.js
@@ -1,13 +1,24 @@
 import { validateToken } from "./tokenValidation";
+import { Headers } from "node-fetch";
 
-const injectHeaders = async (baseOptions, requestHeaders, kubeconfig, app) => {
-  const username = await validateToken(requestHeaders.authorization, app);
+const injectHeaders = async (baseOptions = {}, requestHeaders, kubeconfig, app) => {
+  if (!baseOptions.headers) baseOptions.headers = {};
 
-  baseOptions.headers = { ...baseOptions.headers, "Impersonate-User": username };
+  const injectedHeaders = new Headers();
+  const { groups, email } = await validateToken(requestHeaders.authorization, app);
 
-  kubeconfig.applyAuthorizationHeader(baseOptions);
+  if (groups && groups.length) {
+    injectedHeaders.set("Impersonate-Group", groups[0]); // add groups impersonate (first one must be done via "set")
+    if (groups.length > 1)
+      groups.slice(1, groups.length).forEach((g) => injectedHeaders.append("Impersonate-Group", g)); // add groups impersonate (others must be done via "append")
+  }
 
-  return baseOptions;
+  injectedHeaders.set("Impersonate-User", email); // add email impersonate
+
+  kubeconfig.applyAuthorizationHeader(baseOptions); // add token (to baseOptions.headers because it has to be a pure object)
+  Object.entries(baseOptions.headers).forEach(([key, value]) => injectedHeaders.set(key, value)); // add whatever is in the baseOptions.headers
+
+  return { ...baseOptions, headers: injectedHeaders }; // swap baseOptions.headers with the new headers
 };
 
 export default injectHeaders;
